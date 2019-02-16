a386a36810b7b42fdafa9ee6bf91df5957d37612
ArvinSingh1209 <38300391+ArvinSingh1209@users.noreply.github.com>
2019-02-16 18:49:59 -0500

    Printing Schrodinger's Equation

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import sympy as sp
-x,y,z,t = sp.symbols("x y z t")
-u,v,w,f,g,psi,Psi = sp.symbols("x y z t psi Psi", cls = sp.Function)
-k,c = sp.symbols("k c", constant = True)
+from IPython.display import display
 
+x,y,z,t = sp.symbols("x y z t") # Variables
+
+V,f,g,psi,Psi = sp.symbols("V f g psi Psi", cls = sp.Function) # Functions
+
+k,c,hbar,m = sp.symbols("k c hbar m", constant = True) # Constants
+# hbar = h/(2*sp.pi)
+
+potential = V(x,t) #Time-Dependent Potential
+wavefunc = Psi(x,t) #Time-Dependent Wave Function
+
+SE = sp.Eq(sp.I*hbar*wavefunc.diff(t),hbar**2/(2*m) * wavefunc.diff(x,2) + potential*wavefunc) # Time-Dependent Schrodinger Equation
+
+sp.init_printing(use_latex = True)
+
+display(SE)
